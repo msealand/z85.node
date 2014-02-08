f4eeeb05e1616619df4abd2009866344a941b36b
Michael Sealand <msealand@gmail.com>
2014-02-08 11:44:19 -0800

    Clean up tests a little

--- test/decode.test.js
+++ test/decode.test.js
@@ -2,52 +2,50 @@ var should = require('should'),
 	z85 = require('../'),
 	utils = require('./utils');
 	
-describe('z85', function() {
-	describe('#decode', function() {
-		it('should decode HelloWorld correctly', function() {
-			var string = "HelloWorld"
+describe('z85.decode()', function() {
+	it('should decode HelloWorld correctly (http://rfc.zeromq.org/spec:32#toc3)', function() {
+		var string = "HelloWorld"
+			result = z85.decode(string);
+		
+		should.exist(result);
+		result.should.be.a.Buffer;
+		result.should.eql(new Buffer([0x86, 0x4F, 0xD2, 0x6F, 0xB5, 0x59, 0xF7, 0x5B]));
+	});
+
+	it('should decode a random valid string whose length is divisible by 5', function() {
+		// Test a few of these
+		for (var i = 1; i <= 100; i++) {
+			var r = Math.floor(Math.random() * 10) + 1;
+				string = utils.randomValidString(r * 5);
 				result = z85.decode(string);
-			
+	
 			should.exist(result);
 			result.should.be.a.Buffer;
-			result.should.eql(new Buffer([0x86, 0x4F, 0xD2, 0x6F, 0xB5, 0x59, 0xF7, 0x5B]));
-		});
-	
-		it('should decode a valid string whose length is divisible by 5', function() {
-			// Test a few of these
-			for (var i = 1; i <= 100; i++) {
-				var r = Math.floor(Math.random() * 10) + 1;
-					string = utils.randomValidString(r * 5);
-					result = z85.decode(string);
-		
-				should.exist(result);
-				result.should.be.a.Buffer;
-				result.should.have.length(string.length * 4 / 5);
-			}
-		});
-	
-		it('should not decode a valid string whose length is not divisible by 5', function() {
-			// Test a few of these
-			for (var i = 1; i <= 100; i++) {
-				var string = "";
-				while ((string.length % 5) == 0) {
-					string = utils.randomValidString();
-				}
-			
-				var result = z85.decode(string);
-			
-				should.not.exist(result);
-			}
-		});
+			result.should.have.length(string.length * 4 / 5);
+		}
+	});
 
-		it('should not decode an invalid string of any length', function() {
-			// Test a few of these
-			for (var i = 1; i <= 10; i++) {
-				var string = utils.randomInvalidString(),
-					result = z85.decode(string);
-			
-				should.not.exist(result);
+	it('should not decode a random valid string whose length is not divisible by 5', function() {
+		// Test a few of these
+		for (var i = 1; i <= 100; i++) {
+			var string = "";
+			while ((string.length % 5) == 0) {
+				string = utils.randomValidString();
 			}
-		});
+		
+			var result = z85.decode(string);
+		
+			should.not.exist(result);
+		}
+	});
+
+	it('should not decode an random invalid string of any length', function() {
+		// Test a few of these
+		for (var i = 1; i <= 10; i++) {
+			var string = utils.randomInvalidString(),
+				result = z85.decode(string);
+		
+			should.not.exist(result);
+		}
 	});
 });
--- test/encode.test.js
+++ test/encode.test.js
@@ -2,42 +2,58 @@ var should = require('should'),
 	z85 = require('../'),
 	utils = require('./utils');
 	
-describe('z85', function() {
-	describe('#encode', function() {
-		it('should encode HelloWorld correctly', function() {
-			var bytes = new Buffer([0x86, 0x4F, 0xD2, 0x6F, 0xB5, 0x59, 0xF7, 0x5B]);
+describe('z85.encode()', function() {
+	it('should encode HelloWorld correctly (http://rfc.zeromq.org/spec:32#toc3)', function() {
+		var bytes = new Buffer([0x86, 0x4F, 0xD2, 0x6F, 0xB5, 0x59, 0xF7, 0x5B]);
+			result = z85.encode(bytes);
+		
+		should.exist(result);
+		result.should.be.a.String;
+		result.should.eql("HelloWorld");
+	});
+	
+	it('should encode 0x00 0x00 0x00 0x00', function() {
+		var bytes = new Buffer([0x00, 0x00, 0x00, 0x00]),
+			result = z85.encode(bytes);
+
+		should.exist(result);
+		result.should.be.a.String;
+		result.should.have.length(bytes.length * 5 / 4);
+	});
+	
+	it('should encode 0xFF 0xFF 0xFF 0xFF', function() {
+		var bytes = new Buffer([0xFF, 0xFF, 0xFF, 0xFF]),
+			result = z85.encode(bytes);
+
+		should.exist(result);
+		result.should.be.a.String;
+		result.should.have.length(bytes.length * 5 / 4);
+	})
+
+	it('should encode a random buffer whose length is divisible by 4', function() {
+		// Test a few of these
+		for (var i = 1; i <= 100; i++) {
+			var r = Math.floor(Math.random() * 10) + 1;
+				bytes = utils.randomBuffer(r * 4);
 				result = z85.encode(bytes);
-			
+	
 			should.exist(result);
 			result.should.be.a.String;
-			result.should.eql("HelloWorld");
-		});
-	
-		it('should encode a buffer whose length is divisible by 4', function() {
-			// Test a few of these
-			for (var i = 1; i <= 100; i++) {
-				var r = Math.floor(Math.random() * 10) + 1;
-					bytes = utils.randomBuffer(r * 4);
-					result = z85.encode(bytes);
+			result.should.have.length(bytes.length * 5 / 4);
+		}
+	});
+
+	it('should not encode a random buffer whose length is not divisible by 4', function() {
+		// Test a few of these
+		for (var i = 1; i <= 100; i++) {
+			var bytes;
+			while (!bytes || ((bytes.length % 4) == 0)) {
+				bytes = utils.randomBuffer();
+			}
 		
-				should.exist(result);
-				result.should.be.a.String;
-				result.should.have.length(bytes.length * 5 / 4);
-			}
-		});
-	
-		it('should not encode a buffer whose length is not divisible by 4', function() {
-			// Test a few of these
-			for (var i = 1; i <= 100; i++) {
-				var bytes;
-				while (!bytes || ((bytes.length % 4) == 0)) {
-					bytes = utils.randomBuffer();
-				}
-			
-				var result = z85.encode(bytes);
-			
-				should.not.exist(result);
-			}
-		});
+			var result = z85.encode(bytes);
+		
+			should.not.exist(result);
+		}
 	});
 });
